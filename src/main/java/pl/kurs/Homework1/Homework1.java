767e084dd64c74578443836593940a2cbd4de942
KamilGrzywacz <grzywacz.kam@gmail.com>
2022-10-26 20:23:42 +0200

    test

--- src/main/java/pl/kurs/Homework1/Homework1.java
+++ src/main/java/pl/kurs/Homework1/Homework1.java
@@ -1,9 +1,7 @@
 package pl.kurs.Homework1;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+
 import java.util.Scanner;
-import java.util.spi.AbstractResourceBundleProvider;
 
 public class Homework1 {
     public static void main(String[] args) {
@@ -21,7 +19,6 @@ public class Homework1 {
         }
 
 
-        //int[] array = {10, 20, 30, 40, 50};
         int maxValueFromArray = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] > maxValueFromArray) {
@@ -39,5 +36,14 @@ public class Homework1 {
         System.out.println("Najmniejszy element talibcy to :" + minValueFromArray);
 
 
+        int[] factors = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+        int factor = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % factor == 0) {
+                factor = array[i];
+            }
+
+        }
+        System.out.println("Liczba z największa liczbą dzielników to: " + factor);
     }
 }
